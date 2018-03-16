6f1ecba1e5f7add7fc40642476bcc15b582b78ea
Roman Yasinovskyy <yasiro01@luther.edu>
2018-03-16 14:06:40 -0500

    Update client/server project

--- vpn/client.py
+++ vpn/client.py
@@ -7,18 +7,26 @@ HOST = gethostname()
 PORT = 4390
 
 def main():
-    server_addr = gethostname()
+    # Create a TCP socket
     client_sckt = socket(AF_INET, SOCK_STREAM)
-    client_sckt.connect((server_addr, PORT))
+    # Connect to the server on the same machine
+    client_sckt.connect((HOST, PORT))
+    # Print a status message
+    print('Connected to {}:{}'.format(HOST, PORT))
 
     while True:
+        # Read user input
         msg_out = input('Enter message: ')
-        if msg_out == 'BYE':
+        # Close the connection
+        if msg_out == '\\quit':
+            client_sckt.close()
             break
-
+        # Send a message
         client_sckt.send(msg_out.encode())
-        msg_in = client_sckt.recv(2048)
-        print(msg_in.decode())
+        # Receive a response
+        msg_in = client_sckt.recv(1024)
+        # Print the response
+        print(msg_in.decode('utf-8'))
 
 if __name__ == '__main__':
     main()
--- vpn/server.py
+++ vpn/server.py
@@ -1,22 +1,34 @@
 #!/usr/bin/env python3
 #encoding: UTF-8
 
-from socket import socket, gethostname
+from socket import socket, gethostname, AF_INET, SOCK_STREAM
 
 HOST = gethostname()
 PORT = 4390
 
 def main():
-    server_sckt = socket()
+    # Create a TCP soclet
+    server_sckt = socket(AF_INET, SOCK_STREAM)
+    # Bind the program to a port
     server_sckt.bind((HOST, PORT))
+    # Become a server socket
+    server_sckt.listen(5)
+    # Print a status message
     print('Listening on {}:{}'.format(HOST, PORT))
-    server_sckt.listen()
-
-    conn, _ = server_sckt.accept()
+    # Accept a connection
+    conn, client = server_sckt.accept()
+    print('Connected to {}:{}'.format(client[0], client[1]))
 
     while True:
-        msg_in = conn.recv(2048)
-        msg_out = 'Server says {}'.format(msg_in)
+        # Receive a message
+        msg_in = conn.recv(1024)
+        # Close the connection
+        if len(msg_in) < 1:
+            conn.close()
+            break
+        # Manipulate the message
+        msg_out = 'Server says: {}'.format(msg_in.decode('utf-8')[::-1])
+        # Send a response
         conn.send(msg_out.encode())
 
 if __name__ == '__main__':
